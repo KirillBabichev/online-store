96a91eeb2cf3fe373514d1ab2ce871b6c3099d2c
k.babichev <kirill.babichev@gmail.com>
2023-01-05 11:53:01 +0100

    Create user role check logic

--- server /controllers/userController.js
+++ server /controllers/userController.js
@@ -1,19 +1,58 @@
 const ApiError = require('../error/ApiError');
-class UserController {
-    async registration(req, res) {
+const bcrypt = require('bcrypt');
+const {User, Basket} = require('../models/models');
+const jwt = require('jsonwebtoken');
 
+const generateJWT = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+        );
+}
+
+class UserController {
+    async registration(req, res, next) {
+        const {email, password, role} = req.body;
+        if(!email || !password) {
+            return next(ApiError.badRequest('Incorrect email or password'));
+        }
+
+        const candidate = await User.findOne({where:{email}});
+
+        if(candidate) {
+            return next(ApiError.badRequest('User with same email already exists'));
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        const basket = await Basket.create({userId: user.id});
+        const token = generateJWT(user.id, user.email, user.role);
+
+        return res.json({token});
     }
 
-    async login(req, res) {
-        
+    async login(req, res, next) {
+        const {email, password} = req.body;
+        const user = await User.findOne({where: {email}});
+
+        if(!user) {
+            return next(ApiError.internal('User not found'));
+        }
+
+        const comparePassword = bcrypt.compareSync(password, user.password);
+
+        if(!comparePassword){
+            return next(ApiError.internal('Incorrect email or password'));
+        }
+
+        const token = generateJWT(user.id, user.email, user.role);
+        return res.json({token});
     }
 
     async check(req, res, next) {
-        const {id} = req.query
-        if(!id) {
-            return next(ApiError.badRequest("Bad request"));
-        }
-        res.json(id);
+        const token = generateJWT(req.user.id, req.user.email, req.user.role);
+        return res.json({token});
     }
 }
 
